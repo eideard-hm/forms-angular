cb30030d93e573167220430b89521794696a1e71
eideard-hm <ehhernandez81@misena.edu.co>
2022-06-10 14:48:59 -0500

    Reactive form

--- src/app/forms/reactive/reactive.component.ts
+++ src/app/forms/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-reactive',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reactive.component.scss']
 })
 export class ReactiveComponent implements OnInit {
+  contactForm!: FormGroup
 
-  constructor() { }
+  constructor (private readonly fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit (): void {
+    this.contactForm = this.startForm()
+    this.onPatchValue()
   }
 
+  get contactFormControls (): { [key: string]: AbstractControl } {
+    return this.contactForm.controls
+  }
+
+  startForm (): FormGroup {
+    return this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      checkAdult: [false, [Validators.required]],
+      department: [''],
+      comment: ['', [Validators.required]]
+    })
+  }
+
+  onPatchValue():void{
+    this.contactForm.patchValue({
+      name: 'Edier'
+    })
+  }
+
+  save (): void {
+    if(this.contactForm.invalid){
+      return
+    }
+    console.log('Save reactive form ->', this.contactForm.value)
+  }
 }
--- src/app/forms/reactive/reactive.module.ts
+++ src/app/forms/reactive/reactive.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
 import { ReactiveComponent } from './reactive.component';
-
 
 @NgModule({
   declarations: [
@@ -11,7 +11,8 @@ import { ReactiveComponent } from './reactive.component';
   ],
   imports: [
     CommonModule,
-    ReactiveRoutingModule
+    ReactiveRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class ReactiveModule { }
